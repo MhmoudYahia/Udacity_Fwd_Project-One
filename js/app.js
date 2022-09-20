0d08fe1c8df8551a80cfa53e4c6560952a59dee7
محمود يحيي شوقي عبدالخالق <14712020101567@stud.cu.edu.eg>
2022-09-20 22:51:32 +0200

    responsive done

--- js/app.js
+++ js/app.js
@@ -9,17 +9,17 @@ const sectionsList = [...document.querySelectorAll(".section")];
 //add class active to the viewed section
 window.onscroll = function () {
   // offset Y for the window
-  let windowOffsetTop = window.pageYOffset;
+  const windowOffsetTop = window.pageYOffset;
 
   for (const section of sectionsList) {
     // offset Y for the section element
-    let sectionOffsetTop = section.offsetTop;
+    const sectionOffsetTop = section.offsetTop;
 
     //section all height
-    sectionHeight = section.offsetHeight;
+    const sectionHeight = section.offsetHeight;
 
     //window height
-    windowHeight = window.innerHeight;
+    const windowHeight = window.innerHeight;
 
     //set class active if the section is all on the view
     if (
@@ -38,7 +38,8 @@ linksNav.forEach((link, index) => {
   //add the event
   link.addEventListener("click", (e) => {
     //scroll to function
-    window.scrollTo(0, sectionsList[index].offsetTop);
+    //38 =>> nav height
+    window.scrollTo(0, sectionsList[index].offsetTop - 38);
 
     //prevent defualt action
     e.preventDefault();
