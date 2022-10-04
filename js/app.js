0998bf4420e7f59e1859bb35c6f88d69b433f7e2
محمود يحيي شوقي عبدالخالق <14712020101567@stud.cu.edu.eg>
2022-10-04 12:46:26 +0200

    f

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 // "use strict";
 
-// select links(nodeList)
-const linksNav = [...document.querySelectorAll("nav ul li a")];
-
 //select sections list
 const sectionsList = [...document.querySelectorAll(".section")];
+
+/// create nav elements
+const navUl = document.querySelector("nav ul");
+
+for (let index = 0; index < sectionsList.length; index++) {
+  //create the element and add it to the nav
+  const navElement = document.createElement("li");
+  navElement.textContent = `Section ${index + 1}`;
+  navUl.appendChild(navElement);
+}
+
+// select links(nodeList)
+const linksNav = [...document.querySelectorAll("nav ul li")];
 
 //select topbutton
 const topButton = document.querySelector(".scrollTop");
